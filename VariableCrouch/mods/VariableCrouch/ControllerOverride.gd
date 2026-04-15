74704b8bb1cc934fcaa2322fcdfcb3f3b706577f
Michael Remley <Mikejamesremley@gmail.com>
2026-04-14 19:42:11 -0700

    Unify the two variant mods.

--- VariableCrouch/mods/VariableCrouch/ControllerOverride.gd
+++ VariableCrouch/mods/VariableCrouch/ControllerOverride.gd
@@ -6,6 +6,7 @@ const suppressAimAction := "rail_movement"
 
 var crouchLevel: int = 1
 var crouchHeld: bool = false
+var scrolledWhileHeld: bool = false
 var stashedEvents: Dictionary = {}
 var suppressedRailOptics: Dictionary = {}
 
@@ -15,6 +16,8 @@ func _input(event):
 		return
 	if gameData.freeze or gameData.isFlying or not is_on_floor():
 		return
+
+	scrolledWhileHeld = true
 
 	var prevLevel: int = crouchLevel
 	if event.button_index == MOUSE_BUTTON_WHEEL_UP:
@@ -42,12 +45,16 @@ func _input(event):
 
 func Crouch(delta):
 	var held: bool = Input.is_action_pressed("crouch")
+
 	if held and not crouchHeld:
+		scrolledWhileHeld = false
 		for action in suppressedActions:
 			stashedEvents[action] = InputMap.action_get_events(action)
 			InputMap.action_erase_events(action)
 		Input.action_press(suppressAimAction)
 	elif not held and crouchHeld:
+		if not scrolledWhileHeld:
+			_toggleCrouch()
 		for action in suppressedActions:
 			for evt in stashedEvents.get(action, []):
 				InputMap.action_add_event(action, evt)
@@ -65,14 +72,43 @@ func Crouch(delta):
 			suppressedRailOptics[optic] = optic.railMovement
 			optic.railMovement = false
 
-	var cachedPelvisY: float = pelvis.position.y
+	var cached_is_crouching: bool = gameData.isCrouching
+	var cached_stand_disabled: bool = standCollider.disabled
+	var cached_crouch_disabled: bool = crouchCollider.disabled
+	var cached_pelvis_y: float = pelvis.position.y
+	var cached_crouch_impulse: float = crouchImpulse
+	var cached_stand_impulse: float = standImpulse
 	super.Crouch(delta)
-	pelvis.position.y = cachedPelvisY
+	gameData.isCrouching = cached_is_crouching
+	standCollider.disabled = cached_stand_disabled
+	crouchCollider.disabled = cached_crouch_disabled
+	pelvis.position.y = cached_pelvis_y
+	crouchImpulse = cached_crouch_impulse
+	standImpulse = cached_stand_impulse
+
 	if gameData.isCrouching and crouchLevel == crouchTargets.size():
 		crouchLevel = 1
 	elif not gameData.isCrouching and crouchLevel != crouchTargets.size():
 		crouchLevel = crouchTargets.size()
+
 	pelvis.position.y = lerp(pelvis.position.y, crouchTargets[crouchLevel - 1], delta * 5.0)
+
+func _toggleCrouch():
+	if gameData.isCrouching:
+		if above.is_colliding():
+			return
+		gameData.isCrouching = false
+		standCollider.disabled = false
+		crouchCollider.disabled = true
+		if crouchLevel == 1:
+			standImpulse = 0.1
+		crouchLevel = crouchTargets.size()
+	else:
+		gameData.isCrouching = true
+		standCollider.disabled = true
+		crouchCollider.disabled = false
+		crouchLevel = 1
+		crouchImpulse = 0.1
 
 func _getActiveOptic():
 	if rigManager == null or rigManager.get_child_count() == 0:
